09c5f2359feba2f26a6e51e36cac610f728ba6bf
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2023-09-19 21:57:11 +0530

    Update Rotate_the_Array.java

--- Lec13/Rotate_the_Array.java
+++ Lec13/Rotate_the_Array.java
@@ -1,10 +1,25 @@
 package Lec13;
-
-public class Rotate_the_Array {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+// Leetcode code https://leetcode.com/submissions/detail/1051740442/ (Rajesh  Submission Id)
+class Solution {
+    public static void reverse(int [] arr, int s, int e){
+        
+        while(s<=e){
+            
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        
+        
+       k= k%nums.length;
+    
+      reverse(nums, 0, nums.length-1);
+      reverse(nums, 0, k-1);
+      reverse(nums, k, nums.length-1);
+        
+    }
 }
