0e69ed0c524a88a6bf18e3a7f52b9945ae74d0fc
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2023-12-30 21:22:06 +0530

    Update Sum.java

--- Tree/Sum.java
+++ Tree/Sum.java
@@ -1,31 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int countNodes(TreeNode root) {
+class BinaryTree
+{
+    static int sumBT(Node root){
+        //Code
         
         if(root==null){
             
             return 0;
         }
-        
-        int left_count=  countNodes(root.left);
-        int right_count=  countNodes(root.right);
-        
-        
-        return left_count+right_count+1;
-        
+      
+      int left_sum= sumBT(root.left);
+      int right_sum=sumBT(root.right);
+      
+      return left_sum+right_sum+ root.data;
+      
     }
 }
